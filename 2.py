b409be270f0c23d42511ab64bcf0aaa345e61f2d
= <=>
2023-04-08 03:05:32 +0530

    completed 1 to 2 on 8th april 23

--- 2.py
+++ 2.py
@@ -9,8 +9,24 @@ sys.setrecursionlimit(10**7)
 mod = 1000000007
 
 
+def f(current,arr,dp):
+    # base case
+    if current == 0:
+        return 0
+    else:
+        if dp[current] != -1:
+            return dp[current]
+        one_step = f(current - 1,arr,dp) + abs(arr[current] - arr[current - 1])
+        second_step = mod
+        if current - 2 >= 0:
+            second_step = f(current - 2,arr,dp) + abs(arr[current] - arr[current - 2])
+        result = min(one_step,second_step)
+        dp[current] = result
+        return result
+
 def frogJump(n: int, heights: List[int]) -> int:
-    pass
+    dp = [-1] * (n + 1)
+    return f(n-1,heights,dp)
 
 
 test = int(input())
